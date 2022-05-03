b56aa8e151316b04542c60e8ad790b36c45ae8c5
FlutLab.io <>
2022-05-03 14:20:15 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(CarrerasApp());
+} //Funcion principal
+
+class CarrerasApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Aplicación UACJ',
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: PaginaInicial());
+  } //Widget
+} //Clase CarrerasApp Widget sin estado
+
+class PaginaInicial extends StatefulWidget {
+  PaginaInicial({Key? key}) : super(key: key);
+  @override
+  _PaginaInicialState createState() => _PaginaInicialState();
+} //Widget con estado
+
+class _PaginaInicialState extends State<PaginaInicial> {
+  List<String> images = [
+    "assets/images/pc.jpg",
+    "assets/images/auto.jpg",
+    "assets/images/derecho.jpg",
+    "assets/images/dise.jpg",
+    "assets/images/pc.jpg",
+    "assets/images/auto.jpg",
+    "assets/images/derecho.jpg",
+    "assets/images/dise.jpg",
+    "assets/images/pc.jpg",
+    "assets/images/auto.jpg",
+    "assets/images/derecho.jpg",
+    "assets/images/dise.jpg",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Tutorial GridView"),
+        ),
+        body: GridView.custom(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+          ),
+          childrenDelegate: SliverChildBuilderDelegate(
+            (BuildContext, index) {
+              return Image.asset(
+                images[index],
+                fit: BoxFit.cover,
+              );
+            },
+            childCount: 12,
+          ),
+          padding: EdgeInsets.all(10),
+          shrinkWrap: true,
+        ));
+  }
+}
